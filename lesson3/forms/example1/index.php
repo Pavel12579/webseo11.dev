cd8473b78d3d39b4e66c1fab60bb0da2068f2789
Вадим <guts.vadim@gmail.com>
2016-02-15 19:53:04 +0200

    form

---
+++ lesson3/forms/example1/index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Gutsulyak Vadim <guts.vadim@gmail.com>
+ */
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sum Form</title>
+</head>
+<body>
+
+<form action="sum.php">
+    <input type="number" name="var1"> <br><br>
+    <input type="number" name="var2"> <br><br>
+
+    <input type="submit" value="Sum">
+</form>
+
+</body>
+</html>
