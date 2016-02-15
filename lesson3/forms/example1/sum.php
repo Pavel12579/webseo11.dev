ec550e848ff5551991d0703a1a1065c846d1a1be
Вадим <guts.vadim@gmail.com>
2016-02-15 20:02:25 +0200

    sum script refactor

--- lesson3/forms/example1/sum.php
+++ lesson3/forms/example1/sum.php
@@ -3,9 +3,14 @@
  * @author Gutsulyak Vadim <guts.vadim@gmail.com>
  */
 
-echo $_GET['var1'] + $_GET['var2'];
+$var1 = $_GET['var1'];
+$var2 = $_GET['var2'];
+
+$result = $var1 + $var2;
 
 ?>
+
+<h1><?=$result?></h1>
 
 <br>
 <a href="index.php">Go Back</a>
