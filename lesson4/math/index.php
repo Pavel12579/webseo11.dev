dd15fee796c81cf4668644bb59343a620f55b191
Вадим <guts.vadim@gmail.com>
2016-02-17 21:19:37 +0200

    Math

--- lesson4/math/index.php
+++ lesson4/math/index.php
@@ -6,3 +6,9 @@
 echo "PI: " . M_PI . '<br>';
 echo 'Pages: ' . ceil(55 / 10) . '<br>';
 echo '123123 to hex = ' . dechex(123123) . '<br>';
+echo 'Current date: ' . date('d - F - Y - l') . '<br>';
+
+$pageCount = 10;
+$currentPage = 5;
+
+echo sprintf('Page %d of %d', $currentPage, $pageCount);
