4bfeaff83efe986909a0e673da2fa87e75cd8947
Вадим <guts.vadim@gmail.com>
2016-02-17 20:00:17 +0200

    Math

--- lesson4/math/index.php
+++ lesson4/math/index.php
@@ -3,4 +3,6 @@
  * @author Gutsulyak Vadim <guts.vadim@gmail.com>
  */
 
-echo "PI: " . M_PI;
+echo "PI: " . M_PI . '<br>';
+echo 'Pages: ' . ceil(55 / 10) . '<br>';
+echo '123123 to hex = ' . dechex(123123) . '<br>';
